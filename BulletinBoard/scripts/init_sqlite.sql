e662563b2551fbdafb0cbc6d6b2fb178aab27c83
blueskyson <blueskyson1401@gmail.com>
2022-07-21 15:38:10 +0800

    Update README.md

--- BulletinBoard/scripts/init_sqlite.sql
+++ BulletinBoard/scripts/init_sqlite.sql
@@ -6,20 +6,6 @@ CREATE TABLE Users (
   Password TEXT NOT NULL
 );
 
-INSERT INTO Users (
-  Name, 
-  DisplayName,
-  RegisterDate,
-  Password
-) VALUES (
-  'Lin',
-  'Jack',
-  '2022-07-18 00:00:00',
-  '1234'
-);
-
-SELECT * FROM Users;
-
 CREATE TABLE Posts (
   Id INTEGER PRIMARY KEY AUTOINCREMENT,
   UserId INTEGER NOT NULL,
@@ -27,18 +13,6 @@ CREATE TABLE Posts (
   Text TEXT NOT NULL,
   FOREIGN KEY(UserId) REFERENCES Users(Id)
 );
-
-INSERT INTO Posts (
-  UserId, 
-  SubmitTime,
-  Text
-) VALUES (
-  '1',
-  '2022-07-18 00:01:00',
-  'Hello World!'
-);
-
-SELECT * FROM Posts;
 
 CREATE TABLE Replies (
   Id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -49,17 +23,3 @@ CREATE TABLE Replies (
   FOREIGN KEY(PostId) REFERENCES Posts(Id),
   FOREIGN KEY(UserId) REFERENCES Users(Id)
 );
-
-INSERT INTO Replies (
-  PostId,
-  UserId,
-  SubmitTime,
-  Text
-) VALUES (
-  '1',
-  '1',
-  '2022-07-18 00:01:00',
-  'A reply from Jack!'
-);
-
-SELECT * FROM Replies;
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Bulletin Board
+
+## How to Use
+
+Initialize database:
+
+```
+> cd BulletinBoards
+> sqlite3 .db
+sqlite> .read scripts/init_sqlite.sql
+sqlite> .exit
+```
+
+Run the appication:
+
+```
+> cd BulletinBoard
+> dotnet run
+```
+
+## Demo
+
+- Login Page:
+  ![](./images/login.png)
+- Register Page:
+  ![](./images/register.png)
+- Bulletin Board:
+  ![](./images/bulletinboard.png)
+- Reply a Post:
+  ![](./images/reply.png)
