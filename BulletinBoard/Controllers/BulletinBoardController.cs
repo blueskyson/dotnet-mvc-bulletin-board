27ce1186802bfd7148c7fd3d28083991c8173721
blueskyson <blueskyson1401@gmail.com>
2022-07-22 11:40:41 +0800

    Separate UserController from BulletinBoardController

--- BulletinBoard/Controllers/BulletinBoardController.cs
+++ BulletinBoard/Controllers/BulletinBoardController.cs
@@ -16,28 +16,4 @@ public class BulletinBoardController : Controller {
         List<PostWithDisplayName> viewModel = await _dbContext.GetAllPostsWithDisplayNamesAsync();
         return View(viewModel);        
     }
-
-    public IActionResult ChangeDisplayName() {
-        return View();        
-    }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    [TypeFilter(typeof(FormValidationAttribute), Arguments = new object[] {"DisplayName"})]
-    public IActionResult ChangeDisplayName(String DisplayName) {
-        int? userId = HttpContext.Session.GetInt32(SessionKeys.UserId);
-        if (userId == null) {
-            ViewData["DisplayName"] = "No such user. Log in again may fix the problem";
-            return View();
-        }
-
-        User? currentUser = _dbContext.GetUserById((int)userId!);
-        currentUser!.DisplayName = DisplayName;
-        if (!_dbContext.UpdateUser(currentUser)) {
-            ViewData["DisplayName"] = "Error changing name";
-            return View();
-        }
-        HttpContext.Session.SetString(SessionKeys.DisplayName, DisplayName);
-        return RedirectToAction(nameof(Index));
-    }
 }
---
+++ BulletinBoard/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using BulletinBoard.Models;
+using BulletinBoard.Infrasructure;
+using BulletinBoard.Utils;
+
+namespace BulletinBoard.Controllers;
+
+[ServiceFilter(typeof(AuthorizationAttribute))]
+public class UserController : Controller {
+    private readonly IDbContext _dbContext;
+
+    public UserController(IDbContext context) {
+        _dbContext = context;
+    }
+
+    public IActionResult ChangeDisplayName() {
+        return View();        
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [TypeFilter(typeof(FormValidationAttribute), Arguments = new object[] {"DisplayName"})]
+    public IActionResult ChangeDisplayName(String DisplayName) {
+        int? userId = HttpContext.Session.GetInt32(SessionKeys.UserId);
+        if (userId == null) {
+            ViewData["DisplayName"] = "No such user. Log in again may fix the problem";
+            return View();
+        }
+
+        User? currentUser = _dbContext.GetUserById((int)userId!);
+        currentUser!.DisplayName = DisplayName;
+        if (!_dbContext.UpdateUser(currentUser)) {
+            ViewData["DisplayName"] = "Error changing name";
+            return View();
+        }
+        HttpContext.Session.SetString(SessionKeys.DisplayName, DisplayName);
+        return RedirectToAction("Index", "BulletinBoard");
+    }
+}
