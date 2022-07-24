ca2e6768051b34c8bc159c85bd0b5767f8c6b40f
blueskyson <blueskyson1401@gmail.com>
2022-07-25 00:08:18 +0800

    Add Repositories and Unit of Work

---
+++ BulletinBoard/Models/Repositories/GenericRepository.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BulletinBoard.Models.Repositories;
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity>
+    where TEntity : class
+{
+    private readonly DbContext _context;
+
+    public GenericRepository(DbContext context)
+    {
+        _context = context;
+    }
+
+    public void Add(TEntity entity)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException("entity");
+        }
+
+        _context.Set<TEntity>().Add(entity);
+    }
+
+    public async Task<ICollection<TEntity>> GetAllAsync()
+    {
+        return await _context.Set<TEntity>().ToListAsync();
+    }
+
+    public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+    }
+
+    public void Remove(TEntity entity)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException("entity");
+        }
+
+        _context.Entry(entity).State = EntityState.Deleted;
+    }
+
+    public void Update(TEntity entity)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException("entity");
+        }
+
+        _context.Entry(entity).State = EntityState.Modified;
+    }
+}
